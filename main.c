8e4960c573c4b211fbb1f1489c3d1f398500f41c
iclalakman <163682519+iclalakman@users.noreply.github.com>
2024-03-17 22:02:52 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int base,height,area;
+
+    base=10;
+    height=12;
+    area = (base*height)/2;
+
+    printf("Area of triangle is %d",area);
+
+    return 0;
+}
